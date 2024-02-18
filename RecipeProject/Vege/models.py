df54077f7fc36c996a28164d20e9893fa80a3664
rohit <rohitshahi581@gmail.com>
2024-02-19 02:18:49 +0530

    ..

--- RecipeProject/Vege/models.py
+++ RecipeProject/Vege/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class recipe(models.Model):
+    user = models.ForeignKey(User , on_delete = models.SET_NULL , null = True , blank = True)
     recipe_name = models.CharField(max_length = 100)
     recipe_desc = models.TextField()
     recipe_img = models.ImageField(upload_to= "recipeImages")
